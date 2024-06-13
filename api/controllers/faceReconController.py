f71290643f342012917986a78ca32466ae3e4801
Josué Quesada <josuequesadaxd@gmail.com>
2024-06-12 21:27:03 -0600

    uploaded face recon pkl

---
+++ api/controllers/faceReconController.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pickle
+
+def load_model():
+    with open("api/modelos/faceRecon.pkl", "rb") as f:
+        loaded_model = pickle.load(f)
+    return loaded_model
+
+def predict_Face(data):
+    loaded_model = load_model()
+    new_df = pd.DataFrame(data)
+    # prediction = loaded_model.predict(new_df)
+    # print(prediction)
+    # predicted_price = prediction[0][0]
+    # return f"Según sus parámetros, el precio del vehículo es de {predicted_price:.2f}"
